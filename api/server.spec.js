ddbae705bf81ab62bc9f9e0f3db3dbf463bb6c66
Julie Jonak <juliejonak10@gmail.com>
2019-02-08 20:44:52 -0600

    Added boilerplate

---
+++ api/server.spec.js
@@ -0,0 +1,16 @@
+//two tests per route
+//test what is being returned
+
+//get all
+//get by id
+//404 if id doesn't exist
+//200 if get is successful
+
+
+//check that name is unique before posting
+//return 400 if not
+//201 if post is successful
+
+//for delete, check it exists
+//404 if it doesn't
+//200 if successful
