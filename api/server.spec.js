6d3427b49628205839c2dc39af38522292680221
Julie Jonak <juliejonak10@gmail.com>
2019-02-08 21:41:37 -0600

    Beginning TDD: - server.spec.js: Adding beforeEach and afterEach to handle seeding test db - Model.spec.js: Tests written for four main helper functions

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,56 @@
+const request = require('supertest');
+const server = require('./server');
+const db = require('../data/dbConfig');
+
+describe('the route handlers', () => {
+
+    beforeEach( () => {
+        knex.migrate.rollback()
+            .then( () => {
+                knex.migrate.latest()
+                    .then( () => {
+                        return knex.seed.run()
+                    })
+            })
+    })
+
+    afterEach(async () => {
+        await knex.migrate.rollback()
+    });
+
+
+    
+    describe('get /', () => {
+
+        it('responds with 200', async () => {
+            const response = await request(server).get('/')
+
+            expect(response.status).toBe(200)
+        })
+
+        it('sends the correct response', async () => {
+            const response = await request(server).get('/')
+
+            expect(response.body.length).toEqual(4)
+        })
+
+    })
+
+    describe('get /:id', () => {
+
+    })
+
+    describe('post to /', () => {
+
+    })
+
+    describe('delete /:id', () => {
+
+    })
+
+})
+
+
 //two tests per route
 //test what is being returned
 
--- rebelgirls/rebelgirlsModel.spec.js
+++ rebelgirls/rebelgirlsModel.spec.js
@@ -1,2 +1,51 @@
-//create
-//delete
+const rebelgirls = require('./rebelgirlsModel');
+const db = require('../data/dbConfig.js');
+
+describe('the rebel girls model', () => {
+
+    beforeEach( () => {
+        knex.migrate.rollback()
+            .then( () => {
+                knex.migrate.latest()
+                    .then( () => {
+                        return knex.seed.run()
+                    })
+            })
+    })
+
+    afterEach(async () => {
+        await knex.migrate.rollback()
+    });
+
+
+    it('should fetch all the rebel girls', async () => {
+        const girls = await rebelgirls.fetch()
+
+        expect(girls.length).toBe(4)
+        
+    })
+
+    it('should fetch a rebel girl by id', async () => {
+        const girls = await rebelgirls.fetch(1)
+
+        expect(girls.name).toBe(`Kate O'Malley`)
+
+    })
+
+    it('should add a new rebel girl', async () => {
+        const girls = await rebelgirls.insert({
+            name: 'Misty Copeland',
+            occupation: "Ballerina"
+        })
+
+        expect(girls.length).toBe(5);
+    })
+
+    it('should delete an existing rebel girl by id', async () => {
+        const deletion = await rebelgirls.remove(1);
+
+        expect(deletion.name).toBe(`Kate O'Malley`);
+    })
+
+
+})
